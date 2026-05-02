e76c21ca0370b4ecbe8a5cb5c2399f3a87de6eda
JAYSHI. <Raki.albani2004@gmail.com>
2026-05-02 17:16:10 +0700

    make website lightweight

--- src/components/TechCarousel.tsx
+++ src/components/TechCarousel.tsx
@@ -8,7 +8,7 @@ export default function TechCarousel() {
       <div className="absolute left-0 top-0 bottom-0 w-12 md:w-24 bg-gradient-to-r from-background to-transparent z-10 pointer-events-none" />
       <div className="absolute right-0 top-0 bottom-0 w-12 md:w-24 bg-gradient-to-l from-background to-transparent z-10 pointer-events-none" />
       
-      <div className="flex animate-scroll w-max hover:[animation-play-state:paused]">
+      <div className="flex animate-scroll w-max hover:[animation-play-state:paused] [will-change:transform]">
         {[...Array(2)].map((_, i) => (
           <div key={i} className="flex gap-3 pr-3">
             {techStack.map((tech) => (
